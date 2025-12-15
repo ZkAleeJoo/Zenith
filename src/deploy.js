05063d09c605f77153093a2d03f132a1ff272543
ZkAleeJoo <xxaleejooxx@gmail.com>
2025-12-15 10:19:12 -0500

    Update deploy.js

--- src/deploy.js
+++ src/deploy.js
@@ -30,7 +30,7 @@ const rest = new REST().setToken(process.env.TOKEN);
 
 	
 		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+			Routes.applicationGuildCommands(process.env.CLIENT_ID),
 			{ body: commands },
 		);
 
